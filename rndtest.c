d4f21a1e5e62b60d6e80c5bd3610221deb6b3b1e
lurk101 <lurkone0one@gmail.com>
2023-02-12 04:26:46 -0500

    add monobit and runs tests

--- rndtest.c
+++ rndtest.c
@@ -3,7 +3,7 @@
 #include <pico/rand.h>
 #include <pico/stdlib.h>
 
-static uint32_t histogram[16];
+static uint32_t histogram[64];
 
 int main() {
     stdio_init_all();
@@ -18,22 +18,66 @@ int main() {
 
     printf("Using get_rand_64()\n\n");
 
-    for (int pass = 1; pass <= 2; ++pass) {
-        printf("Upper 4 bits - pass %d\n", pass);
-        for (int i = 0; i < 16; ++i)
-            histogram[i] = 0;
-        for (int i = 0; i < 1000000; ++i)
-            ++histogram[get_rand_64() >> 60];
-        for (int i = 0; i < 16; ++i)
-            printf("%2d - %d\n", i, histogram[i]);
+    printf("Upper 4 bits distribution\n");
+    for (int i = 0; i < 16; ++i)
+        histogram[i] = 0;
+    for (int i = 0; i < 1000000; ++i)
+        ++histogram[get_rand_64() >> 60];
+    for (int i = 0; i < 16; ++i)
+        printf("%2d - %d\n", i, histogram[i]);
+    printf("\nLower 4 bits distribution\n");
+    for (int i = 0; i < 16; ++i)
+        histogram[i] = 0;
+    for (int i = 0; i < 1000000; ++i)
+        ++histogram[get_rand_64() & 0xf];
+    for (int i = 0; i < 16; ++i)
+        printf("%2d - %d\n", i, histogram[i]);
+    printf("\nSingle bit distribution\n");
+    int ones = 0, zeros = 0;
+    for (int i = 0; i < 1000000; ++i) {
+        int64_t n = get_rand_64();
+        for (int j = 0; j < 64; ++j) {
+            if (n < 0)
+                ++ones;
+            else
+                ++zeros;
+            n <<= 1;
+        }
     }
-    for (int pass = 1; pass <= 2; ++pass) {
-        printf("Lower 4 bits - pass %d\n", pass);
-        for (int i = 0; i < 16; ++i)
-            histogram[i] = 0;
-        for (int i = 0; i < 1000000; ++i)
-            ++histogram[get_rand_64() & 0xf];
-        for (int i = 0; i < 16; ++i)
-            printf("%2d - %d\n", i, histogram[i]);
+    printf("zeros = %d\n", zeros);
+    printf("ones  = %d\n", ones);
+    printf("\nRun lengths distributions\n");
+    for (int i = 0; i < 32; ++i)
+        histogram[i] = 0;
+    int last = -1, last_count;
+    for (int i = 0; i < 1000000; ++i) {
+        int64_t n = get_rand_64();
+        for (int j = 0; j < 64; ++j) {
+            int next = n < 0 ? 1 : 0;
+            n <<= 1;
+            if (last < 0) {
+                last = next;
+                last_count = 1;
+                continue;
+            }
+            if (next == last) {
+                ++last_count;
+                continue;
+            }
+            if (last_count > 31)
+                last_count = 31;
+            ++histogram[last * 32 + last_count];
+            last = next;
+            last_count = 1;
+        }
+    }
+    if (last_count > 31)
+        last_count = 31;
+    ++histogram[last * 32 + last_count];
+    double expected = 1000000.0 * 64 / 4;
+    for (int i = 1; i < 32; ++i) {
+        printf("%2d - zeros %-7d  ones %-7d  total %-8d  expected %.0f\n", i, histogram[i],
+               histogram[i + 32], histogram[i] + histogram[i + 32], expected);
+        expected /= 2.0;
     }
 }
