5a75ae02e64785837e8ae3580557fe75754e7242
lurk101 <lurkone0one@gmail.com>
2023-02-12 12:13:50 -0500

    Add standard deviations and document tests

--- rndtest.c
+++ rndtest.c
@@ -1,9 +1,33 @@
+#include <math.h>
 #include <stdio.h>
 
 #include <pico/rand.h>
 #include <pico/stdlib.h>
 
 static uint32_t histogram[64];
+
+static const int ITERATIONS = 1000000;
+
+static double sum;
+static double sq_sum;
+
+static void std_dev_init(void) {
+    sum = 0;
+    sq_sum = 0;
+}
+
+static void std_dev_calc(int v) {
+    sum += v;
+    sq_sum += (double)v * v;
+}
+
+static double std_dev_mean(int n) { return sum / n; }
+
+static double std_dev(int n) {
+    double mean = sum / n;
+    return sqrt(sq_sum / n - mean * mean);
+    return sum / n;
+}
 
 int main() {
     stdio_init_all();
@@ -18,23 +42,42 @@ int main() {
 
     printf("Using get_rand_64()\n\n");
 
+    // Bucket test
+    // The focus of this test is to determine that values are evenly distributed
+    // across the range
     printf("Upper 4 bits distribution\n");
     for (int i = 0; i < 16; ++i)
         histogram[i] = 0;
-    for (int i = 0; i < 1000000; ++i)
+    for (int i = 0; i < ITERATIONS; ++i)
         ++histogram[get_rand_64() >> 60];
-    for (int i = 0; i < 16; ++i)
-        printf("%2d - %d\n", i, histogram[i]);
+    std_dev_init();
+    for (int i = 0; i < 16; ++i) {
+        std_dev_calc(histogram[i]);
+        printf("%2d - %-8d expected %-.0f\n", i, histogram[i], ITERATIONS / 16.0);
+    }
+    printf("Mean      %.2f\n", std_dev_mean(16));
+    printf("Std. dev. %.2f\n", std_dev(16));
+
     printf("\nLower 4 bits distribution\n");
     for (int i = 0; i < 16; ++i)
         histogram[i] = 0;
-    for (int i = 0; i < 1000000; ++i)
+    for (int i = 0; i < ITERATIONS; ++i)
         ++histogram[get_rand_64() & 0xf];
-    for (int i = 0; i < 16; ++i)
-        printf("%2d - %d\n", i, histogram[i]);
+    std_dev_init();
+    for (int i = 0; i < 16; ++i) {
+        std_dev_calc(histogram[i]);
+        printf("%2d - %-8d expected %-.0f\n", i, histogram[i], ITERATIONS / 16.0);
+    }
+    printf("Mean      %.2f\n", std_dev_mean(16));
+    printf("Std. dev. %.2f\n", std_dev(16));
+
+    // Frequency (Monobit) Test
+    // The purpose of this test is to determine whether the number of ones and
+    // zeros in a sequence are approximately the same as would be expected for
+    // a truly random sequence.
     printf("\nSingle bit distribution\n");
     int ones = 0, zeros = 0;
-    for (int i = 0; i < 1000000; ++i) {
+    for (int i = 0; i < ITERATIONS; ++i) {
         int64_t n = get_rand_64();
         for (int j = 0; j < 64; ++j) {
             if (n < 0)
@@ -44,13 +87,26 @@ int main() {
             n <<= 1;
         }
     }
-    printf("zeros = %d\n", zeros);
-    printf("ones  = %d\n", ones);
+    printf("zeros = %-8d  expected %.0f\n", zeros, ITERATIONS * 64.0 / 2);
+    printf("ones  = %-8d  expected %.0f\n", ones, ITERATIONS * 64.0 / 2);
+    std_dev_init();
+    std_dev_calc(zeros);
+    std_dev_calc(ones);
+    printf("Mean      %.2f\n", std_dev_mean(2));
+    printf("Std. dev. %.2f\n", std_dev(2));
+
+    // Runs test
+    // The focus of this test is the total number of runs in the sequence, where
+    // a run is an uninterrupted sequence of identical bits. A run of length k
+    // consists of exactly k identical bits and is bounded before and after with
+    // a bit of the opposite value. The purpose of the runs test is to determine
+    // whether the number of runs of ones and zeros of various lengths is as
+    // expected for a random sequence
     printf("\nRun lengths distributions\n");
     for (int i = 0; i < 32; ++i)
         histogram[i] = 0;
     int last = -1, last_count;
-    for (int i = 0; i < 1000000; ++i) {
+    for (int i = 0; i < ITERATIONS; ++i) {
         int64_t n = get_rand_64();
         for (int j = 0; j < 64; ++j) {
             int next = n < 0 ? 1 : 0;
@@ -74,7 +130,7 @@ int main() {
     if (last_count > 31)
         last_count = 31;
     ++histogram[last * 32 + last_count];
-    double expected = 1000000.0 * 64 / 4;
+    double expected = ITERATIONS * 64.0 / 4;
     for (int i = 1; i < 32; ++i) {
         printf("%2d - zeros %-7d  ones %-7d  total %-8d  expected %.0f\n", i, histogram[i],
                histogram[i + 32], histogram[i] + histogram[i + 32], expected);
